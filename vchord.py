629216ac442e944283c5b7b2f6475a9c5b974a06
George <gkudrayvtsev@live.com>
2017-04-08 16:49:46 -0700

    Adds visual representation.

--- vchord.py
+++ vchord.py
@@ -1,13 +1,18 @@
+import random
+import math
+
 import pygame
 import chord
+import utils
 
-class Visual_ChordNode(ChordNode):
+class Visual_ChordNode(chord.LocalChordNode):
     def __init__(self, data):
         super(Visual_ChordNode, self).__init__(data)
         self.hashFont = pygame.font.SysFont("arial", 12)
         self.hashText = self.hashFont.render("%s" % str(self.hash)[:8], True, (0, 0, 0))
         self.entity   = pygame.Surface((16, 16))
         self.entity.fill((255, 0, 0))
+        self._selected = False
 
     def place(self, x, y):
         self.position = (x, y)
@@ -18,10 +23,20 @@ class Visual_ChordNode(ChordNode):
             self.position[0] + 4,
             self.position[1] + self.entity.get_height()))
 
-def visuals(ring):
-    pygame.init()
-    pygame.font.init()
+    @property
+    def selected(self):
+        return self._selected
 
+    @selected.setter
+    def selected(self, state):
+        if state:
+            print "selected %s" % str(self)
+            self.entity.fill((255, 255, 0))
+        else:
+            print "de-selected %s" % str(self)
+            self.entity.fill((255, 0, 0))
+
+def visuals(ring):
     SCREEN = (800, 800)
     CENTER = ((SCREEN[0] / 2) - 50, SCREEN[1] / 2)
     RADIUS = (SCREEN[0] / 2) - 50
@@ -31,12 +46,14 @@ def visuals(ring):
 
     j = 0
     for i in xrange(0, 360, 360 / len(ring)):
-        x = CENTER[0] + RADIUS * math.cos(rad(i))
-        y = CENTER[1] + RADIUS * math.sin(rad(i))
+        x = CENTER[0] + RADIUS * math.cos(utils.rad(i))
+        y = CENTER[1] + RADIUS * math.sin(utils.rad(i))
         ring[j].place(x, y)
         j += 1
 
         print "(%.1f, %.1f)" % (x, y)
+
+    selected_node = None
 
     quit = False
     while not quit:
@@ -45,11 +62,86 @@ def visuals(ring):
                 quit = True
                 break
 
+            elif event.type == pygame.KEYDOWN and \
+                 event.key == pygame.K_ESCAPE:
+
+                selected_node = None
+                for node in ring:
+                    node.selected = False
+
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                rect = pygame.Rect(event.pos[0], event.pos[1], 1, 1)
+                collider = None
+                for node in ring:
+                    comp_rect = node.entity.get_rect()
+                    comp_rect.x = node.position[0]
+                    comp_rect.y = node.position[1]
+                    if comp_rect.colliderect(rect):
+                        collider = node
+                        break
+
+                if collider and collider != selected_node:
+                    if selected_node is None:
+                        selected_node = collider
+                        collider.selected = True
+
+                    else:
+                        import pdb; pdb.set_trace()
+                        collider.joinRing(selected_node)
+
+        window.fill((255, 255, 255))
         for dot in ring:
             dot.draw(window)
 
-        for finger in ring[0].fingers.nodes:
-            pygame.draw.line(window, (255, 0, 0), ring[0].position, finger.position)
+        if selected_node is not None:
+            #
+            # Draw a line to all of the entries in the finger table.
+            #
+            for finger in selected_node.fingers.entries:
+                if finger.node is None:
+                    continue
+
+                pygame.draw.line(
+                    window,
+                    (255, 0, 0),
+                    selected_node.position,
+                    finger.node.position)
+
+            #
+            # Draw a line to the successor and predecessor.
+            #
+            if selected_node.successor is not None:
+                pygame.draw.line(
+                    window,
+                    (255, 255, 0),
+                    selected_node.position,
+                    selected_node.successor.position)
+
+            if selected_node.predecessor is not None:
+                pygame.draw.line(
+                    window,
+                    (0, 0, 255),
+                    selected_node.position,
+                    selected_node.predecessor.position)
 
         pygame.display.flip()
 
+if __name__ == "__main__":
+    pygame.init()
+    pygame.font.init()
+
+    ring = [ Visual_ChordNode(str(_)) for _ in xrange(18) ]
+
+    try:
+        visuals(ring)
+
+    except KeyboardInterrupt:
+        print "Shutting down background stabilizer threads."
+        for node in ring:
+            node.stable.running = False
+
+        for i, node in enumerate(ring):
+            node.stable.join(500)
+            print "Shut down %d/%d...\r" % (i + 1, len(ring)),
+
+        print
