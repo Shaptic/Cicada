71eb9debbd3afec2ff24fa7f009466f600140a15
George <george.k@berkeley.edu>
2017-05-09 02:25:05 -0700

    Moves utilities properly.

--- protocol/chord_messages.py
+++ protocol/chord_messages.py
@@ -4,27 +4,7 @@ import collections
 from chord.hashring import BITCOUNT
 
 from . import message
-# from . import utils
-
-class utils:
-    @staticmethod
-    def ip_to_int(addr):
-        a, b, c, d = addr.split('.')
-        return sum([
-            int(a) << 24,
-            int(b) << 16,
-            int(c) << 8,
-            int(d)
-        ])
-
-    @staticmethod
-    def int_to_ip(n):
-        return '.'.join([
-            str((n & 0xFF000000) >> 24),
-            str((n & 0x00FF0000) >> 16),
-            str((n & 0x0000FF00) >> 8),
-            str((n & 0x000000FF))
-        ])
+from . import utils
 
 
 class JoinRequest(message.BaseMessage):
